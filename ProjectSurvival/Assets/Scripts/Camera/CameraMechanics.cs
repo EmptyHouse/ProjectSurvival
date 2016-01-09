0da97eaf177a4b2e318cdc9cfa034d9e576fd47e
ryanAndersen05 <ryan.andersen05@gmail.com>
2016-01-09 11:53:22 -0500

    Added in cameraMechanics

---
+++ ProjectSurvival/Assets/Scripts/Camera/CameraMechanics.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMechanics : MonoBehaviour {
+    public float cameraAcceleration;
+    public float maxSpeed;
+
+    Transform cameraFollow;
+    Rigidbody rigid;
+    Vector3 offsetUnitVec;
+    float currentMagnitude;
+
+    void Start()
+    {
+        cameraFollow = transform.parent;
+        transform.parent = null;
+
+        rigid = GetComponent<Rigidbody>();
+        rigid.useGravity = false;
+
+        offsetUnitVec = transform.position - cameraFollow.position;
+
+        currentMagnitude = offsetUnitVec.magnitude;
+        offsetUnitVec = offsetUnitVec.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        updateLocation();
+    }
+
+    void updateLocation()
+    {
+        Vector3 goalLocation = (cameraFollow.position + offsetUnitVec * currentMagnitude);
+        Vector3 goalVelocity = (transform.position - goalLocation).normalized * maxSpeed;
+        rigid.velocity = Vector3.Lerp(rigid.velocity, goalVelocity, Time.fixedDeltaTime * cameraAcceleration);
+    }
+
+    
+}
